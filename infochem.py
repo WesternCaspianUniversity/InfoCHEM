2e2ca0ca34c55bdcc0244d8f6b86d33dae007f98
Emin Gambarli <61322552+EMN503@users.noreply.github.com>
2022-04-05 22:32:23 +0400

    Update infochem.py

--- infochem.py
+++ infochem.py
@@ -1,64 +1,77 @@
-import tkinter
+import os
 from tkinter import *
 from PIL import ImageTk, Image
+
 
 mainwindow = Tk()
 mainwindow.geometry("1100x550")
 mainwindow.title("InfoCHEM")
+mainwindow.configure(bg="#424F55")
+
+
+def imput_rule():
+    os.system("python3 command.py")
+
+def apply():
+    print("I Don't Know Your Name2")
+
+def removerule():
+    print("remove")
+
+def stopbutton():
+    os.system("pkexec ufw disable")
+
+def playbutton():
+    os.system("pkexec ufw enable")
 
 frame = Frame(width=300, height=55)
 frame.pack()
-frame.place(anchor='center', relx=0.18, rely=0.10)
+frame.place(anchor='center', relx=0.18, rely=0.12)
 
-img = ImageTk.PhotoImage(Image.open("InfoChem.png"))
-label = Label(frame, image = img)
+#img = ImageTk.PhotoImage(Image.open("InfoChem.png"))
+label = Label(frame, background="#424F55", text="InfoCHEM", fg="#E67E22", font=("Roboto", 50))
 label.pack()
-
-frame1 = Frame(width=876, height=396)
-frame1.pack()
-frame1.place(anchor='center', relx=0.44, rely=0.56)
-
-img1 = ImageTk.PhotoImage(Image.open("commands.png"))
-label1 = Label(frame1, image = img1)
-label1.pack()
-
-frame2 = Frame(width=93, height=93)
-frame2.pack()
-frame2.place(anchor='center', relx=0.67, rely=0.10)
-
-img2 = ImageTk.PhotoImage(Image.open("Add.png"))
-label2 = Label(frame2, image = img2)
-label2.pack()
-
-frame3 = Frame(width=93, height=93)
-frame3.pack()
-frame3.place(anchor='center', relx=0.77, rely=0.10)
-
-img3 = ImageTk.PhotoImage(Image.open("Remove.png"))
-label3 = Label(frame3, image = img3)
-label3.pack()
 
 frame4 = Frame(width=93, height=93)
 frame4.pack()
-frame4.place(anchor='center', relx=0.91, rely=0.11)
+
+frame4.place(anchor='center', relx=0.91, rely=0.131)
 
 img4 = ImageTk.PhotoImage(Image.open("Shield.png"))
-label4 = Label(frame4, image = img4)
+label4 = Label(frame4, image=img4, bg="#424F55")
 label4.pack()
 
-frame5 = Frame(width=93, height=93)
-frame5.pack()
-frame5.place(anchor='center', relx=0.91, rely=0.40)
+applyimg = ImageTk.PhotoImage(Image.open("Checked.png"))
+button1 = Button(width=93, height=93, image=applyimg, bg="#424F55", command=apply, bd=0)
+button1.pack()
+button1.place(anchor='center', relx=0.91, rely=0.55)
 
-img5 = ImageTk.PhotoImage(Image.open("Checked.png"))
-label5 = Label(frame5, image = img5)
-label5.pack()
+addimg = ImageTk.PhotoImage(Image.open("Add.png"))
+button2 = Button(width=93, height=93, image=addimg, bg="#424F55", command=imput_rule, bd=0)
+button2.pack()
+button2.place(anchor='center', relx=0.67, rely=0.13)
 
-frame6 = Frame(width=93, height=93)
-frame6.pack()
-frame6.place(anchor='center', relx=0.91, rely=0.72)
+removeimg = ImageTk.PhotoImage(Image.open("Remove.png"))
+button3 = Button(width=93, height=93, image=removeimg, bg="#424F55", command=removerule)
+button3.pack()
+button3.place(anchor='center', relx=0.77, rely=0.13)
 
-img6 = ImageTk.PhotoImage(Image.open("Close.png"))
-label6 = Label(frame6, image = img6)
-label6.pack()
+frame2 = Frame(width=800, height=355, bg="#647176")
+frame2.pack()
+frame2.place(anchor='center', relx=0.40, rely=0.58)
+
+#rulelabel = Label(frame2, text="", bg="#DADAFF")
+#rulelabel.pack()
+#rulelabel.place(anchor='center', relx=0.10, rely=0.10)
+
+startimg = ImageTk.PhotoImage(Image.open("play.png"))
+button3 = Button(width=93, height=93, image=startimg, bg="#424F55", command=stopbutton, bd=0)
+button3.pack()
+button3.place(anchor='center', relx=0.91, rely=0.35)
+
+stopimg = ImageTk.PhotoImage(Image.open("stop.png"))
+button3 = Button(width=93, height=93, image=stopimg, bg="#424F55", command=playbutton, bd=0)
+button3.pack()
+button3.place(anchor='center', relx=0.91, rely=0.75)
+
 mainwindow.mainloop()
